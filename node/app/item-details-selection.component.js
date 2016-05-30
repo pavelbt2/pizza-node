712ca6c954ed1006a682c75a266d87a693311fef
Pavel Belenkovich <pavelbt2@gmail.com>
2016-05-30 22:46:45 +0200

    item details selection

---
+++ node/app/item-details-selection.component.js
@@ -0,0 +1,47 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var core_1 = require('@angular/core');
+var router_deprecated_1 = require('@angular/router-deprecated');
+var order_service_1 = require('./order.service');
+var ItemDetailsSelectionComponent = (function () {
+    function ItemDetailsSelectionComponent(router, orderService, routeParams) {
+        this.router = router;
+        this.orderService = orderService;
+        this.routeParams = routeParams;
+    }
+    ItemDetailsSelectionComponent.prototype.ngOnInit = function () {
+        var _this = this;
+        this.orderId = +this.routeParams.get('orderId');
+        this.itemId = this.routeParams.get('itemId');
+        console.info("ItemDetailsSelectionComponent: "
+            + "orderId=" + this.orderId + " itemId=" + this.itemId);
+        this.orderService.getItemList()
+            .then(function (itemList) {
+            _this.item = itemList.find(function (item) { return item.id == _this.itemId; });
+        });
+        return;
+    };
+    ItemDetailsSelectionComponent.prototype.placeOrder = function () {
+    };
+    ItemDetailsSelectionComponent.prototype.goBack = function () {
+        window.history.back();
+    };
+    ItemDetailsSelectionComponent = __decorate([
+        core_1.Component({
+            selector: 's-item-selection',
+            templateUrl: 'app/item-details-selection.component.html',
+        }), 
+        __metadata('design:paramtypes', [router_deprecated_1.Router, order_service_1.OrderService, router_deprecated_1.RouteParams])
+    ], ItemDetailsSelectionComponent);
+    return ItemDetailsSelectionComponent;
+}());
+exports.ItemDetailsSelectionComponent = ItemDetailsSelectionComponent;
+//# sourceMappingURL=item-details-selection.component.js.map
---
+++ node/app/item-details-selection.component.ts
@@ -0,0 +1,49 @@
+import { Component, Input, Output, OnInit, EventEmitter } from '@angular/core';
+import { Router, RouteParams } from '@angular/router-deprecated';
+import { OrderService } from './order.service';
+import { Item } from './item';
+
+@Component({
+	selector: 's-item-selection',
+	templateUrl: 'app/item-details-selection.component.html',
+	//styleUrls: ['app/item-selection.component.css']    
+})
+
+export class ItemDetailsSelectionComponent implements OnInit {
+    
+    orderId : number;
+    itemId : string;
+    item : Item;
+    count : number;
+    details : string;
+    user : string;
+    
+    constructor(private router: Router, private orderService: OrderService, private routeParams: RouteParams) {
+	}
+    
+    ngOnInit() {
+		this.orderId = +this.routeParams.get('orderId');
+        this.itemId = this.routeParams.get('itemId');
+        
+        console.info("ItemDetailsSelectionComponent: "
+            +"orderId="+this.orderId+" itemId="+this.itemId);
+        
+        this.orderService.getItemList()
+        .then(itemList =>
+			{
+				this.item = itemList.find(item => item.id == this.itemId);
+			} 
+		)        
+        
+		return;        
+    }
+    
+    placeOrder() {
+        
+    }
+    
+    goBack() {
+		window.history.back();
+	}    
+
+}
