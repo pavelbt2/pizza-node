1263e2ba28ad524a6da3cbc8e698628e34f483e3
Pavel Belenkovich <pavelbt2@gmail.com>
2016-06-14 22:40:17 +0200

    add order status

---
+++ node/app/order-status.js
@@ -0,0 +1,8 @@
+"use strict";
+var OrderStatus = (function () {
+    function OrderStatus() {
+    }
+    return OrderStatus;
+}());
+exports.OrderStatus = OrderStatus;
+//# sourceMappingURL=order-status.js.map
---
+++ node/app/order-status.ts
@@ -0,0 +1,4 @@
+export class OrderStatus {
+	id : string;
+	pretty : string;
+}
